9bffbd8cb886d1069e50ccdf8b30aec729b81a7f
ACE-b612 <2710072482@qq.com>
2026-06-08 03:26:05 +0000

    添加 NPC_Top.v 顶层模块（初始版本）

---
+++ npc/NPC_Top.v
@@ -0,0 +1,7 @@
+module NPC_Top(
+    input clock,
+    input reset,
+
+    output [31:0] io_pc,
+    output io_is_mmio
+);
--- npc/csrc/src/npc_trace.cpp
+++ npc/csrc/src/npc_trace.cpp
@@ -4,8 +4,8 @@ extern "C" void npc_itrace(int pc, int next_pc, int inst) {
 #ifdef CONFIG_ITRACE
     iringbuf_write(pc, inst);
 
-    // char buf[128];
-    // disassemble(buf, sizeof(buf), pc, (uint8_t *)&inst, 4);
-    // printf("ITRACE: 0x%08x: %08x %s\n", pc, inst, buf);
+    char buf[128];
+    disassemble(buf, sizeof(buf), pc, (uint8_t *)&inst, 4);
+    printf("ITRACE: 0x%08x: %08x %s\n", pc, inst, buf);
 #endif
 }
