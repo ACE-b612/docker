2f1c08b17a5291d3685a47d7c4cecb518de16a44
ACE-b612 <2710072482@qq.com>
2026-06-09 16:36:37 +0000

    step7,8

--- npc/vsrc/NPC_Top.v
+++ npc/vsrc/NPC_Top.v
@@ -31,6 +31,43 @@ MemDPIC imem(
     .rdata(inst)
 );
 
+//step7 解析指令字段
+wire [6:0] opcode;
+wire [4:0] rd;
+wire [2:0] funct3;
+wire [4:0] rs1;
+wire [4:0] rs2;
+wire [6:0] funct7;
+
+assign opcode = inst[6:0];
+assign rd     = inst[11:7];
+assign funct3 = inst[14:12];
+assign rs1    = inst[19:15];
+assign rs2    = inst[24:20];
+assign funct7 = inst[31:25];
+
+//step8 实例化寄存器堆
+wire [31:0] rs1_data;
+wire [31:0] rs2_data;
+
+wire reg_wen;
+wire [31:0] wb_data;
+
+RegFile u_regfile(
+    .clk(clock),
+
+    .wen(reg_wen),
+    .waddr(rd),
+    .wdata(wb_data),
+
+    .raddr1(rs1),
+    .raddr2(rs2),
+
+    .rdata1(rs1_data),
+    .rdata2(rs2_data)
+);
+
+
 ITraceDPIC itrace(
     .clk(clock),
     .pc(pc),
